6f0caf2a5010cffd43e163bcb637cab83f5d50cb
Lisa <psycho000reaper@gmail.com>
2022-07-19 23:18:34 +0300

    up-to-date with Figma

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,12 @@
 import "../styles/Card.css";
-// import avatar from "../img/avatar.png";
-import a from "../avatar.png";
 
 const Card = (props) => {
     return (
         <div className="card-wrapper">
-            <div className="card-avatar-wrapper">
+            <div className="card-avatar-img">
                 <div className={"card-able card-able-" + props.able}>
                     {props.able ? "действующий" : "недействующий"}
                 </div>
-                <img src={a} alt="avatar" className="card-avatar-img"/>
             </div>
             <div className="card-info">
                 <span className="card-info-header">{props.title}</span>
@@ -17,7 +14,7 @@ const Card = (props) => {
             </div>
             <div className="card-button">
                 <div className="card-button-inside">
-                {props.standards} стандартов <a href={props.link}>Перейти</a>
+                    {props.standards} стандартов <a href={props.link}>Перейти</a>
                 </div>
             </div>
         </div>
